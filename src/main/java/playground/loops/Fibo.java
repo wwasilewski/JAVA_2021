210c06cc9a80a481fdbc851b714cd741f8cd1b34
Wojciech <w.wasilewski@gmail.com>
2022-03-17 15:35:15 +0100

    test

--- src/main/java/playground/loops/Fibo.java
+++ src/main/java/playground/loops/Fibo.java
@@ -8,7 +8,7 @@ public class Fibo {
     public static void main(String[] args) {
 
         int[] fibo = new int[30];
-        fibo[0] = 1;
+        fibo[0] = 0;
         fibo[1] = 1;
 
         for (int i = 2; i < fibo.length; i++) {
